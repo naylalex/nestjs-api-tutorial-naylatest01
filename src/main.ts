b3c6899cae12b567061c21d8539d446fe5f55b35
naylatest01 <82061519+naylatest01@users.noreply.github.com>
2022-05-03 23:41:26 +0000

    Configured SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('TC2005/CEM/401 API')
+  .setDescription('A sample API for class')
+  .setVersion('0.1')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
